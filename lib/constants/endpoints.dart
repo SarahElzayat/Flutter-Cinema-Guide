be8212556a77c9066ea833892a17cafc27527fc4
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-28 17:00:16 +0200

    .

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -3,25 +3,3 @@ const CINEMAS = 'cinemas/';
 const MOVIES = 'movies/';
 const GENRES = 'genres/';
 
-const Map<String, dynamic> JSON = {
-  "movie_title": "Avatar : Part 1 (2009)",
-  "movie_description":
-      "A paraplegic ex-marine, Jake Sully, is recruited to operate an \"Avatar,\" a human mind in an alien body, with the highly advanced technology of a corporation seeking to plunder the resources of the planet Pandora. ",
-  "movie_image":
-      "https://media.elcinema.com/uploads/_150x200_b0f3c2ef1407950ea16062ee69df367094b13f82ed342bc953e9a030b8755b00.jpg",
-  "movie_genres": ["Science Fiction", "Action", "Adventure", "Fantasy"],
-  "movie_link_id": "/en/work/2000011/",
-  "movie_rating": 9.1,
-  "cinema": [
-    {
-      "id": 24,
-      "cinema_name": "IMAX Plaza  Cinema",
-      "cinema_link": "https://elcinema.com/en/theater/3101134/"
-    },
-    {
-      "id": 59,
-      "cinema_name": "Grand  ABC Achrafieh Cinema",
-      "cinema_link": "https://elcinema.com/en/theater/3101161/"
-    }
-  ]
-};
