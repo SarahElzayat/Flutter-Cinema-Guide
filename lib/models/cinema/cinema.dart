f7059686c2359dd3bda70341d36070ee92341e0f
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-29 22:40:20 +0200

    cinema Model

--- lib/models/cinema/cinema.dart
+++ lib/models/cinema/cinema.dart
@@ -4,14 +4,24 @@ class Cinema {
   int? id;
   String? cinemaName;
   String? cinemaLink;
+  String? cinemaAddress;
+  String? cinemaImage;
   List<Movie>? movies;
 
-  Cinema({this.id, this.cinemaName, this.cinemaLink, this.movies});
+  Cinema(
+      {this.id,
+      this.cinemaName,
+      this.cinemaLink,
+      this.movies,
+      this.cinemaImage,
+      this.cinemaAddress});
 
   factory Cinema.fromJson(Map<String, dynamic> json) => Cinema(
         id: json['id'] as int?,
         cinemaName: json['cinema_name'] as String?,
         cinemaLink: json['cinema_link'] as String?,
+        cinemaAddress: json['cinema_address'] as String?,
+        cinemaImage: json['cinema_image'] as String?,
         movies: (json['movies'] as List<dynamic>?)
             ?.map((e) => Movie.fromJson(e as Map<String, dynamic>))
             .toList(),
@@ -22,5 +32,7 @@ class Cinema {
         'cinema_name': cinemaName,
         'cinema_link': cinemaLink,
         'movies': movies?.map((e) => e.toJson()).toList(),
+        'cinema_image': cinemaImage,
+        'cinema_address': cinemaAddress,
       };
 }
