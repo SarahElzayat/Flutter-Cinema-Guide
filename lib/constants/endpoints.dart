6096c31d790234bf58b83d1528b7a895ece60f56
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-29 13:22:58 +0200

    end points

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,4 +1,6 @@
 const BASE_URL = 'http://10.0.2.2:8000/api/';
+const BASE_URL_LOCAL = 'http://localhost:8000/api/';
+
 const CINEMAS = 'cinemas/';
 const MOVIES = 'movies/';
 const GENRES = 'genres/';
